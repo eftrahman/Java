2676d5b314d3bedb3a61065d292e8b5e972075a3
eftrahman <83782669+eftrahman@users.noreply.github.com>
2024-06-01 23:37:09 +0600

    1 6 3

---
+++ practice0011/src/practice0011/Practice0011.java
@@ -0,0 +1,45 @@
+package practice0011;
+/**
+ *
+ * @Rinzler 05.04.2022
+ */
+public class Practice0011 {
+
+    public static void main(String[] args) {
+//        double d1 = 11.23d;
+//        float f1 = 12.32f;
+//        int i1 = (int)d1;
+//        int i2=(int)f1;
+//        float f2=(float)d1;
+//        char ch1='A';
+//        int i3 = (int)ch1;
+//        String st1="hello";
+//        double d3 = (double)ch1;
+//        System.out.println("int value : "+i1+" and "+i2+" and "+i3);
+//        System.out.println("float value : "+f2);
+//        System.out.println("Double Value "+d3);
+
+//        boolean a = true;
+//        int b = 0;
+//        while(a){
+//            System.out.println("looping");
+//            b=b+1;
+//            if(b==10){
+//                a=false;
+//            }
+//        }
+//        int c =0;
+//        boolean a2 = false;
+//        do{
+//            System.out.println("looping again");
+//            c=c+1;
+//            if(c==10){
+//                a2=true;
+//            }
+//        }while(!a2);
+
+          
+        
+    }
+    
+}
---
+++ practiece010/src/practiece010/Practiece010.java
@@ -0,0 +1,18 @@
+package practiece010;
+
+/**
+ *
+ * @Rinzler 05.04.2022
+ */
+public class Practiece010 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        double d = a;
+        double d1= 1.12d;
+        int i1= (int)d1;
+        //System.out.println("int "+a);
+        System.out.println("double **"+i1);
+    }
+    
+}
