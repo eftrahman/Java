907b8063148b7dde5562f6128596ef585062f3a6
eftrahman <83782669+eftrahman@users.noreply.github.com>
2024-06-02 23:34:26 +0600

    02 6 5

---
+++ JavaApplication32/src/javaapplication32/JavaApplication32.java
@@ -0,0 +1,20 @@
+
+package javaapplication32;
+
+/**
+ *
+ * @author ALL
+ */
+public class JavaApplication32 {
+
+    public static void main(String[] args) {
+        String ss = "Hello";
+        String aa = new String(ss);
+        System.out.println(ss==aa)+" "+ss.equals(aa));
+        int a ;
+        scanner sc = new scanner;
+         gggggggggggggggggvvv
+    }
+    
+}
+QBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
