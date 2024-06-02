f1c9c920c685ce943e9a0f073e964df4590a59fc
eftrahman <83782669+eftrahman@users.noreply.github.com>
2024-06-02 23:33:41 +0600

    02 6 4

---
+++ JavaApplication31/src/javaapplication31/JavaApplication31.java
@@ -0,0 +1,19 @@
+package javaapplication31;
+
+/**
+ * 05.02.2024
+ * @Rinzler
+ */
+public class JavaApplication31 {
+
+    public static void main(String[] args) {
+        String sss = "Hello";
+        String aaa = "World";
+        System.out.println(sss+aaa);
+        String namex = "Rinz";
+        String namey = "ler";
+        String name = namex+namey;
+        int codeID = 112;
+        System.out.println("Hello "+name +" you code is "+codeID*3);
+    }
+}
